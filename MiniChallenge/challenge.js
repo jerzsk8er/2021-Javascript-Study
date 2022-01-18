ca4eb456237b721b55ab550b32f0695dfb5aa203
jerzsk8er <jayden.cabrera@vtsd.com>
2022-01-18 11:52:04 -0500

    beep

--- MiniChallenge/challenge.js
+++ MiniChallenge/challenge.js
@@ -1,3 +1,8 @@
 /*
 * Design a page that when you change a text input, an image gets bigger
  */
+
+function imgbig(){
+    var img = document.getElementById("img");
+    img.style.width = "100%";
+}
